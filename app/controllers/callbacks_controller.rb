f129c0bc5257a19fb1bcc5769cec36c96067cd58
caiwenhn2008@163.com <caiwenhn2008@163.com>
2015-03-15 10:56:42 +0800

    change github secret for heroku app

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -8,6 +8,11 @@ class CallbacksController < Devise::OmniauthCallbacksController
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user
     end
+
+    def qq_connect
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
     
     def sign_out
         sign_out @user
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :timeoutable,
-    :omniauthable, :omniauth_providers => [:github,:tqq, :weibo]
+    :omniauthable, :omniauth_providers => [:github,:qq_connect, :weibo]
          
     def self.from_omniauth(auth)
         where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
